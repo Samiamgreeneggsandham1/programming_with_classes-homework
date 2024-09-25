d925290c2f8cb3df9dcf448d80b6bc25886e4ddb
Samiamgreeneggsandham1 <thr23002@byui.edu>
2024-09-25 14:33:45 -0600

    I finally figured out what I'm actually supposed to be doing in regards to the pre-set up file system, I feel stupid, but what I've done pretty much works, so there we go. I just finished the guess_numbers assignment, I'll probably re-order all the files and stuff later.

--- sandbox/Sandbox/guess_number.cs
+++ sandbox/Sandbox/guess_number.cs
@@ -1,24 +1,34 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Guess
 {
     public static void RunGuessNumber()
     {
-        Console.Write("What is the magic number? ");
-        string userInput = Console.ReadLine();
-        int magicNumber = int.Parse(userInput);
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
 
-        Console.Write("What is your guess? ");
-        string userInput2 = Console.ReadLine();
-        int guessNumber = int.Parse(userInput);
+        bool isCorrect = false;
 
-        if (guessNumber > magicNumber)
+        while (!isCorrect)
         {
-            Console.WriteLine("Lower");
-        }
-        else if (guessNumber < magicNumber)
-        {
-            Console.WriteLine("Higher");
+            Console.Write("What is your guess? ");
+            string userInput2 = Console.ReadLine();
+            int guessNumber = int.Parse(userInput2);
+
+            if (guessNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guessNumber == magicNumber)
+            {
+                isCorrect = true; // Set the Boolean thing to be true
+                Console.WriteLine("Correct, well played!");
+            }
         }
     }
 }
