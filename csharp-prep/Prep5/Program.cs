92b40b3a277baccd498d395fd038f506d4cdbf33
Samiamgreeneggsandham1 <thr23002@byui.edu>
2024-09-27 11:08:26 -0600

    Created and completed the Prep5 assignment :)

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Net.WebSockets;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        WelcomeMessage(); // Call the function
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squareNumber = SquareNumber(userNumber);
+        DisplayResult(userName, squareNumber);
+
+    }
+
+    static void WelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string userInput = Console.ReadLine();
+        int userNumber = int.Parse(userInput);
+        return userNumber;
+    }
+
+    static int SquareNumber(int userNumber)
+    {
+        int squareNumber = userNumber * userNumber;
+        return squareNumber;
+    }
+    
+    static void DisplayResult(string userName, int squareNumber)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {squareNumber}.");
     }
 }
